55cef0c46f0648d722a06ceb02ff31ec70fa265f
hkorble <94326376+hkorble@users.noreply.github.com>
2024-07-11 14:50:08 -0600

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,2 +1,4 @@
 # At this point I have yet to begin coding, once I have purchased the electronics required I can begin
 # Code will be pushed to here once I have begun
+
+# Since I plan to use a Raspberry Pi, Python makes the most sense 
